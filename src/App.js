431399acd1920e370e9de77e15306eb342325fbe
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-09-26 11:07:21 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Total Budget: 200 Million</h2>
+        <h2>This is our corporate team</h2>
+        <h4>Corporate Teams provides high potential leaders and teams to increase team engagement</h4>
+        <h2>Total Budget: 500 Million</h2>
       </header>
     </div>
   );
